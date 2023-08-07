f57f941f567da158ad4090c6e2c9f46db4ed1085
wulongcha500 <139962074+wulongcha500@users.noreply.github.com>
2023-08-07 09:58:31 +0800

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,13 @@
+public class MaxArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum value is " + max);
+    }
+}
